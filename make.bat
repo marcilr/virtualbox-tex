533c24e51d06b6ff2c3d40bff79179c6896b3708
Raymond E. Marcil <remarcil@alaska.gov>
2019-06-13 13:54:47 -0800

    make.bat   Build PDF from LaTeX source

---
+++ make.bat
@@ -0,0 +1,13 @@
+rem
+rem make.bat
+rem Created Wed Jul 25 10:01:00 AKDT 2007
+rem by Raymond E. Marcil 
+rem
+rem Build LaTeX document using Cygwin.
+rem
+
+rem GNU make under Cygwin
+MAKE=D:\SW\Cygwin\usr\bin\make
+
+rem Build PDF from LaTeX source
+%MAKE%
