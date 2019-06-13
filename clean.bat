7fe1d6e28b1488706d6ec55325605bfea8561fb5
Raymond E. Marcil <remarcil@alaska.gov>
2019-06-13 13:50:59 -0800

    clean.bat   Remove dynamically created files from build process.   Analogous to 'make clean' target.

---
+++ clean.bat
@@ -0,0 +1,12 @@
+rem
+rem clean.bat
+rem Created Wed Jul 25 09:52:17 AKDT 2007
+rem by Raymond E. Marcil 
+rem
+rem Remove dynamically created files from build process.
+rem Analogous to 'make clean' target.
+rem
+
+
+rem Delete log, postscript, temp, etc files
+del *.aux *.dvi *.lof *.log *.lot *.out *.ps *.toc *.tmp *~
